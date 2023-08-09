ed4416ca0600ec50003ba106be838782f9bfc18c
Jerry Barrows-Fitzgerald <jbarrowsfitzgerald@gmail.com>
2023-08-08 22:53:18 -0500

    fix: Adjusted footer, login components

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,11 @@
 export default function Footer(standData) {
   return (
     <footer className='w-full bg-green-500'>
-      <h3 className='p-2 text-center'>{standData.length} Locations Worlwide</h3>
+      <h3 className='p-2 text-center'>
+        {standData.length > 0
+          ? `${standData.length} Locations Worldwide`
+          : 'No locations to report'}
+      </h3>
     </footer>
   );
 }
--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,20 +1,45 @@
 export default function LoginForm({ onLogin }) {
   async function handleSubmit(e) {
     e.preventDefault();
+    console.log(e);
     onLogin(e.target.username.value, e.target.password.value);
   }
 
   return (
-    <>
-      <div class='flex flex-col w-1/2'>
-        <form onSubmit={handleSubmit} class='bg-green-200 m-2 rounded-sm'>
-          <label htmlFor='username'>User Name</label>
-          <input type='text' name='username' id='username' />
-          <label htmlFor='password'>Password</label>
-          <input type='password' name='password' id='password' />
-          <button type='submit'>Log In</button>
-        </form>
-      </div>
-    </>
+    <form
+      class='flex flex-col w-1/2 items-center bg-green-200 m-2 p-4 rounded-sm'
+      onSubmit={handleSubmit}
+    >
+      <label
+        class='w-full text-center uppercase text-xl font-bold my-4'
+        htmlFor='username'
+      >
+        User Name
+      </label>
+      <input
+        class='w-full text-center text-xl my-4 p-2'
+        type='text'
+        name='username'
+        id='username'
+      />
+      <label
+        class='w-full text-center uppercase text-xl font-bold my-4'
+        htmlFor='password'
+      >
+        Password
+      </label>
+      <input
+        class='w-full text-center text-xl my-4 p-2'
+        type='password'
+        name='password'
+        id='password'
+      />
+      <button
+        class='bg-green-600 w-full text-xl uppercase p-2 my-6 font-bold'
+        type='submit'
+      >
+        Log In
+      </button>
+    </form>
   );
 }
